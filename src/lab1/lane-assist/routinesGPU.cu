0509a16781547850b09745961f55bbc17044cb34
Olivier Martin <olivier@labapart.com>
2023-03-24 14:10:03 +0100

    entrega lab1

---
+++ src/lab1/lane-assist/routinesGPU.cu
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+
+#include "routinesGPU.h"
+
+void line_asist_GPU(uint8_t *im, int height, int width,
+	uint8_t *imEdge, float *NR, float *G, float *phi, float *Gx, float *Gy, uint8_t *pedge,
+	float *sin_table, float *cos_table, 
+	uint32_t *accum, int accu_height, int accu_width,
+	int *x1, int *x2, int *y1, int *y2, int *nlines)
+{
+
+	/* To do */
+}
