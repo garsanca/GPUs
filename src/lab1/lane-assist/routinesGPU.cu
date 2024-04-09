800fd12d9c7b91bf060225d8bc28850ec61663ec
Carlos GS <garsanca@ucm.es>
2024-04-09 13:43:09 +0200

    error detected in routinesGPU.cu

--- src/lab1/lane-assist/routinesGPU.cu
+++ src/lab1/lane-assist/routinesGPU.cu
@@ -6,7 +6,7 @@
 #include "routinesGPU.h"
 
 void lane_assist_GPU(uint8_t *im, int height, int width,
-	int *x1, int *x2, int *y1, int *y2, int *nlines)
+	int *x1, int *y1, int *x2, int *y2, int *nlines)
 {
 
 	/* To do */
